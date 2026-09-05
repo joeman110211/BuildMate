7ee34a4395d658267a8e014a3c100518b48d87e8
joeman110211 <123514195+joeman110211@users.noreply.github.com>
2026-09-05 15:00:44 +0100

    Lint browser E2E tests without requiring Playwright in app bundle

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,11 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   { ignores: ['dist/**', '.expo/**', 'node_modules/**'] },
+  {
+    files: ['e2e/**/*.cjs'],
+    rules: {
+      'import/no-unresolved': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
 ]);
